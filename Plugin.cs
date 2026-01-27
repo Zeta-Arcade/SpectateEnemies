d19f755159fbdce6b2967997b3052cb50da810d1
Zeta Arcade <zZETAz@outlook.com>
2026-01-27 10:49:56 +0000

    Fixed filepath references for the deps

--- Plugin.cs
+++ Plugin.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 
 namespace SpectateEnemy;
 
-[BepInPlugin("SpectateEnemy", "SpectateEnemy", "2.8.0")]
+[BepInPlugin("SpectateEnemy", "SpectateEnemy", "2.8.1")]
 [BepInDependency("com.rune580.LethalCompanyInputUtils", BepInDependency.DependencyFlags.HardDependency)]
 internal class Plugin : BaseUnityPlugin
 {
